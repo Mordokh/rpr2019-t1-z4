530712c7e725bdcf488f4cc38b42849e84eafa39
Ajdin <ahadzic5@etf.unsa.ba>
2019-10-14 18:36:34 +0200

    Initial commit

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr;
 
-public class Artikl {
+public class Artikl
+{
+    private String naziv, kod;
+    private int cijena;
+
+
+    public Artikl (String naziv, int cijena, String kod)
+    {
+        this.naziv=naziv;
+        this.cijena=cijena;
+        this.kod=kod;
+    }
+
+    public final String getNaziv(){return naziv;}
+    public final String getKod() {return kod;}
+    public final int getCijena() {return cijena;}
+
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr;
+import java.util.ArrayList;
 
-public class Supermarket {
+public class Supermarket
+{
+    public final static int max_value=1000;
+    private Artikl[] sve =new Artikl[max_value];
+    public void dodajArtikl (Artikl a)
+    {
+
+    }
+
 }
